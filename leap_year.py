29c1313eeeaea84db70d2e22b19f020360734b6c
Huy Nguyen <huynguyensupper@gmail.com>
2021-03-07 15:29:10 -0800

    Finish up the program with user inputs

--- leap_year.py
+++ leap_year.py
@@ -3,3 +3,19 @@ def IsLeap(year):
         return True
     else:
         return False
+
+if __name__ == "__main__":
+    
+    while True:
+        try:
+            year = int(input("Please enter a year: "))
+            if year < 0:
+                print("Please try again with valid year.")
+            else:
+                if IsLeap(year) == True:
+                    print("It's a leap year!")
+                else:
+                    print("It's not a leap year."
+                )
+        except:
+            print("Please input valid input")
