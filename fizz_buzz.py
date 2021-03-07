f8f46898a49d62e888d7987e54e9e4f6fbbc2c6a
Huy Nguyen <huynguyensupper@gmail.com>
2021-03-07 10:15:36 -0800

    Fixed the code for the test

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,2 +1,10 @@
-def fizz_buzz():
-    pass
+def FizzBuzz(n):
+    if n % 15 == 0:
+        return "FizzBuzz"
+    elif n % 5 == 0:
+        return "Buzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    else:
+        return n
+
